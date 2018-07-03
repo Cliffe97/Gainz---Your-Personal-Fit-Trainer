19297bd72e6192075d638d9d19c0227c93535252
mklleung <mkleung@brandeis.edu>
2018-07-03 16:47:07 -0400

    Added a page for creating a workout-no content

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const createWorkoutController = require('./controllers/createWorkoutController')
 
 var app = express();
 
@@ -21,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.get('/createWorkout',
+            createWorkoutController.renderMain)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ controllers/createWorkoutController.js
@@ -0,0 +1,6 @@
+'use strict'
+
+exports.renderMain =  (req,res) => {
+  //console.log("in the swController.renderMain")
+  res.render('createWorkout')
+}
