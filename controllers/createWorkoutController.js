a227661f69dfb21b0e6082cdb8e883ed45524c50
mklleung <mkleung@brandeis.edu>
2018-07-03 17:27:21 -0400

    Making the addable forms

--- controllers/createWorkoutController.js
+++ controllers/createWorkoutController.js
@@ -4,3 +4,7 @@ exports.renderMain =  (req,res) => {
   //console.log("in the swController.renderMain")
   res.render('createWorkout')
 }
+exports.addMoreWorkout = (req, res) => {
+  console.log("in addMoreWorkout")
+
+};
