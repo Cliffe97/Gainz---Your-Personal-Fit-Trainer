e5fcdd58ec10f2433ba38d1a9d02d07d92ba6b7b
mklleung <mkleung@brandeis.edu>
2018-07-10 11:04:58 -0400

    started doing the controllers for mongo

--- controllers/repWorkoutController.js
+++ controllers/repWorkoutController.js
@@ -4,28 +4,8 @@ console.log("loading the createRepWorkout Controller")
 
 exports.renderMain =  (req,res) => {
   //console.log("in the swController.renderMain")
-  res.render('createRepWorkout')
+  res.render('repWorkout')
 }
-
-exports.saveRepWorkout = ( req, res ) => {
-  console.log("in saveRepWorkout!")
-  console.dir(req)
-  let repWorkout = new RepWorkout( {
-    workoutName: req.body.workoutName,
-    repetitions: req.body.repetitions,
-    setsAndRounds: req.body.setsAndRounds
-  } )
-
-  console.log("repWorkout = "+repWorkout)
-
-  repWorkout.save()
-    .then( () => {
-      res.redirect( '/createRepWorkout' );
-    } )
-    .catch( error => {
-      res.send( error );
-    } );
-};
 
 exports.getAllRepWorkout = ( req, res ) => {
   console.log('in getAllRepWorkout')
