1ed8c2a27cb6451dd5f73bc8904a520c237156ba
mklleung <mkleung@brandeis.edu>
2018-07-10 12:57:42 -0400

    further changes from my branch

--- controllers/createIntervalWorkoutController.js
+++ controllers/createIntervalWorkoutController.js
@@ -1,10 +1,45 @@
 'use strict'
+const IntervalWorkout = require( '../models/AddIntervalWorkout' );
+console.log("loading the createIntervalWorkout Controller")
 
 exports.renderMain =  (req,res) => {
   //console.log("in the swController.renderMain")
   res.render('createIntervalWorkout')
 }
-exports.addMoreIntervalWorkout = (req, res) => {
-  console.log("in addMoreWorkout")
 
+exports.saveIntervalWorkout = ( req, res ) => {
+  console.log("in saveIntervalWorkout!")
+  console.dir(req)
+  let intervalWorkout = new IntervalWorkout( {
+    workoutName: req.body.workoutName,
+    timeDuration: req.body.timeDuration
+  } )
+
+  console.log("intervalWorkout = "+ intervalWorkout)
+
+  intervalWorkout.save()
+    .then( () => {
+      res.redirect( '/createIntervalWorkout' );
+    } )
+    .catch( error => {
+      res.send( error );
+    } );
+};
+
+exports.getAllIntervalWorkout = ( req, res ) => {
+  console.log('in getAllIntervalWorkout')
+  IntervalWorkout.find( {} )
+    .exec()
+    .then( ( intervalWorkout ) => {
+      res.render( 'intervalWorkout', {
+        intervalWorkout: intervalWorkout
+      } );
+    } )
+    .catch( ( error ) => {
+      console.log( error.message );
+      return [];
+    } )
+    .then( () => {
+      console.log( 'IntervalWorkout promise complete' );
+    } );
 };
--- controllers/createRepWorkoutController.js
+++ controllers/createRepWorkoutController.js
@@ -1,10 +1,46 @@
 'use strict'
+const RepWorkout = require( '../models/AddRepWorkout' );
+console.log("loading the createRepWorkout Controller")
 
 exports.renderMain =  (req,res) => {
   //console.log("in the swController.renderMain")
   res.render('createRepWorkout')
 }
-exports.addMoreRepWorkout = (req, res) => {
-  console.log("in addMoreWorkout")
 
+exports.saveRepWorkout = ( req, res ) => {
+  console.log("in saveRepWorkout!")
+  console.dir(req)
+  let repWorkout = new RepWorkout( {
+    workoutName: req.body.workoutName,
+    repetitions: req.body.repetitions,
+    setsAndRounds: req.body.setsAndRounds
+  } )
+
+  console.log("repWorkout = "+repWorkout)
+
+  repWorkout.save()
+    .then( () => {
+      res.redirect( '/createRepWorkout' );
+    } )
+    .catch( error => {
+      res.send( error );
+    } );
+};
+
+exports.getAllRepWorkout = ( req, res ) => {
+  console.log('in getAllRepWorkout')
+  RepWorkout.find( {} )
+    .exec()
+    .then( ( repWorkout ) => {
+      res.render( 'repWorkout', {
+        repWorkout: repWorkout
+      } );
+    } )
+    .catch( ( error ) => {
+      console.log( error.message );
+      return [];
+    } )
+    .then( () => {
+      console.log( 'RepWorkout promise complete' );
+    } );
 };
