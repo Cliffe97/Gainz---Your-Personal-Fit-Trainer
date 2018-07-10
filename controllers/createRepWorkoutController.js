14322b049e163f1199cf5814141e52157bf8759a
mklleung <mkleung@brandeis.edu>
2018-07-10 10:37:10 -0400

    Added models and started adding mongo to form controllers

--- controllers/createRepWorkoutController.js
+++ controllers/createRepWorkoutController.js
@@ -1,10 +1,28 @@
 'use strict'
+const RepWorkout = require( '../models/AddRepWorkout' );
+console.log("loading the createRepWorkout Controller")
 
 exports.renderMain =  (req,res) => {
   //console.log("in the swController.renderMain")
   res.render('createRepWorkout')
 }
-exports.addMoreRepWorkout = (req, res) => {
-  console.log("in addMoreWorkout")
 
+exports.saveRepWorkout = ( req, res ) => {
+  console.log("in saveRepWorkout!")
+  console.dir(req)
+  let repWorkout = new RepWorkout( {
+    workoutName: req.body.workoutName,
+    repetitions: req.body.repetitions,
+    setsAndRounds: req.body.setsAndRounds
+  } )
+
+  console.log("repWorkout = "+repWorkout)
+
+  repWorkout.save()
+    .then( () => {
+      res.redirect( '/createRepWorkout' );
+    } )
+    .catch( error => {
+      res.send( error );
+    } );
 };
