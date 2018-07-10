b01f1801958ecec026e7b2769f56fff60c929640
gavinyhan <gavinyhan@gmail.com>
2018-07-10 10:43:38 -0400

    home page: background image

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const createRepWorkoutController = require('./controllers/createRepWorkoutContro
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
