35b380b20d9dac87b18183be3028094e316b2deb
mklleung <mkleung@brandeis.edu>
2018-07-10 11:56:22 -0400

    resolving git merging

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const intervalWorkoutController = require('./controllers/intervalWorkoutControll
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
