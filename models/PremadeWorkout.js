19c8b73b0bcf6eb0f6a25d6b1fdb2f812dcba24d
Cliffe97 <34017648+Cliffe97@users.noreply.github.com>
2018-07-15 17:39:25 -0400

    11
    11

---
+++ models/PremadeWorkout.js
@@ -0,0 +1,17 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+
+
+var workoutSchema = mongoose.Schema( {
+  Name: String,
+  Maker: String,
+  Duration: String,
+  Difficulty: String,
+  BodyFocus: String,
+  TrainingType: String,
+  Equipment: String,
+  Content: String,
+  Timing: String
+} );
+
+module.exports = mongoose.model( 'PremmadeWorkout', workoutSchema );
