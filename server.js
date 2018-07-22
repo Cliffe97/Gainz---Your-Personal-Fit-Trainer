cb4d0e78999535f2e2518a70dc72eb9788bf23fb
Cliffe97 <34017648+Cliffe97@users.noreply.github.com>
2018-07-22 15:29:06 -0400

    added find method

--- server.js
+++ server.js
@@ -57,13 +57,52 @@ obj.from.path('Workout.csv').to.array(function (data) {
     //console.log(constrains)
 });
 
+function get_count(category){
+  count = 0;
+  var workout_category;
+  console.log("category: "+ category);
+  console.log(MyData.length);
+  for(var index = 0; index < MyData.length; index ++){
+    if(category = MyData[index]["category"]){
+      console.log((count+1)+"result found");
+      selectedName[0] = MyData[index]["name"]
+      selectedWorkout[0] = MyData[index]["workout1"]
+      selectedWorkout[1] = MyData[index]["workout2"]
+      selectedWorkout[2] = MyData[index]["workout3"]
+      selectedWorkout[3] = MyData[index]["workout4"]
+      selectedWorkout[4] = MyData[index]["workout5"]
+      selectedWorkout[5] = MyData[index]["workout6"]
+      selectedWorkout[6] = MyData[index]["workout7"]
+      selectedTimer[0] = MyData[index]["timer1"]
+      selectedTimer[1] = MyData[index]["timer2"]
+      selectedTimer[2] = MyData[index]["timer3"]
+      selectedTimer[3] = MyData[index]["timer4"]
+      selectedTimer[4] = MyData[index]["timer5"]
+      selectedTimer[5] = MyData[index]["timer6"]
+      selectedTimer[6] = MyData[index]["timer7"]
+      selectedRec [0] = MyData[index]["rec1"]
+      selectedRec [1] = MyData[index]["rec2"]
+      selectedRec [2] = MyData[index]["rec3"]
+      selectedRec [3] = MyData[index]["rec4"]
+      selectedRec [4] = MyData[index]["rec5"]
+      selectedRec [5] = MyData[index]["rec6"]
+      selectedRec [6] = MyData[index]["rec7"]
+      count ++;
+    }
+  }
+  return count;
+}
+
 function process_request(req, res){
   var output_string = "there was an error";
   var temp = "";
   console.log("in the processing")
 
-  if (req.body.queryResult.intent.name == "projects/newagent-2d1f9/agent/intents/97790d1d-312f-4c72-8ef1-d71861981704"){
-    output_string = "testing the backend";
+  if(req.body.queryResult.intent.name == "projects/newagent-2d1f9/agent/intents/0c2900e5-d161-43b5-b96e-a7433219dc64"){
+    console.log("how many triggered");
+    var category = req.body.queryResult.parameters["BodyFocus"];
+    var result = get_count(category);
+    output_string = "There are"+result;
   } else if (req.body.queryResult.intent.name == "projects/newagent-2d1f9/agent/intents/afdd2389-1dd0-4b95-9feb-d031e59e1912"){
     console.log("in the start intent")
     selectedName[0] = MyData[1]["name"]
@@ -113,7 +152,7 @@ function process_request(req, res){
 };
 
 service.post('/hook', function(req, res){
-    console.log(req)
-    console.log(JSON.stringify(req.body, null, 2));
+  //  console.log(req)
+  //  console.log(JSON.stringify(req.body, null, 2));
     process_request(req, res)
 })
