0506d844632a1a3ea3e6b738ef37edccede612ce
mklleung <mkleung@brandeis.edu>
2018-07-30 11:04:01 -0400

    fixed heroku callback

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@ module.exports = {
     'googleAuth' : {
         'clientID': '171377849743-u5otlud0kj0l5042dr5apm9gqj2ag0b7.apps.googleusercontent.com',
         'clientSecret'  : 'yULjxE6SJ1_Beg5yYMG1VGxD',
-        'callbackURL'   : 'http://localhost:3000/login/authorized'
-        //'callbackURL'   : 'https://humangainz.herokuapp.com/login/authorized'
+        //'callbackURL'   : 'http://localhost:3000/login/authorized'
+        'callbackURL'   : 'https://humangainz.herokuapp.com/login/authorized'
         //'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6100/login/authorized'
     }
 
