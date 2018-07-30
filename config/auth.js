f6d450d73a51f5dc398b65c2f2c163385ab700a1
mklleung <mkleung@brandeis.edu>
2018-07-30 11:02:56 -0400

    fixed the google auth

--- config/auth.js
+++ config/auth.js
@@ -2,9 +2,10 @@ module.exports = {
 
 
     'googleAuth' : {
-        'clientID': '587771107463-c7bpes52akbr4mc6ste8nfcif9q1icks.apps.googleusercontent.com',
-        'clientSecret'  : 'RZoGoUNeMjWmQlz9fPeWfgxt',
+        'clientID': '171377849743-u5otlud0kj0l5042dr5apm9gqj2ag0b7.apps.googleusercontent.com',
+        'clientSecret'  : 'yULjxE6SJ1_Beg5yYMG1VGxD',
         'callbackURL'   : 'http://localhost:3000/login/authorized'
+        //'callbackURL'   : 'https://humangainz.herokuapp.com/login/authorized'
         //'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6100/login/authorized'
     }
 
